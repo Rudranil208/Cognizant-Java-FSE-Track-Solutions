2f2ab91f60b7b7b952f168ffbfff6e311b1e8b54
Rudranil <124135986+Rudranil208@users.noreply.github.com>
2026-06-25 23:27:06 +0530

    Delete week 1/module 1/Main.java

--- week 1/module 1/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Logger obj1=Logger.instanceCreator();
-        Logger obj2=Logger.instanceCreator();
-
-        System.out.println(obj1==obj2);
-    }
-}
